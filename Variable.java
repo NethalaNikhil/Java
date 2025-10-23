31051c310affc0f62858d6bdf1dd6cceda83dba6
NethalaNikhil <20b91a12d7@srkrec.ac.in>
2025-10-24 00:25:16 +0530

    Difference b/w Jvm and jre,jdk variable

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable{
+    public static void main(String [] args)
+    {
+        int number = 6;
+        String name = "Nikhil";
+        System.out.println(number);
+        System.out.println(name);
+    }
+}
